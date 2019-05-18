7837a4fe27d257f21efa800263c0b9d13c067046
Rishikesh Sharat <rishikeshcn@gmail.com>
2019-05-18 21:04:35 +0530

    New files

---
+++ graph/dfs.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include <iostream>
+#include <stack>
+#include <vector>
+
+void dfs(long x, long n, vector<long> g[])
+{
+    bool vis[n] = { 0 };
+    stack<long> s;
+    long a, b, i;
+    s.push_back(x);
+    while (!s.empty()) {
+        a = s.top();
+        vis[a] = 1;
+        b = g[a].size();
+        for (i = 0; i < b; i++)
+            if (!vis[g[a][i]])
+                s.push_back(g[a][i]);
+    }
+}
